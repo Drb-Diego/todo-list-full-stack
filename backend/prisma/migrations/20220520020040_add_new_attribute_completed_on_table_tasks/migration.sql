1269961f5621cdeb4ac3ac20070b088e35ff78f6
Diego Brito <diegorbrito9@gmail.com>
2022-05-19 23:10:26 -0300

    new: adicionando o campo "completed" na tabela tasks

---
+++ backend/prisma/migrations/20220520020040_add_new_attribute_completed_on_table_tasks/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - Added the required column `completed` to the `tasks` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `tasks` ADD COLUMN `completed` BOOLEAN NOT NULL;
